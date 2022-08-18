333294fb6e7a4457232d2388f257015bc2dfd492
Viktor Hirenko <33980925+monsara@users.noreply.github.com>
2022-08-18 17:48:43 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template><BaseInput v-model="inputValue" /> {{ inputValue }}</template>
+
+<script>
+import BaseInput from "./components/BaseInput.vue";
+
+export default {
+  components: {
+    BaseInput,
+  },
+  data() {
+    return {
+      inputValue: "",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <input
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
